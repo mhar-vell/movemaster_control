f3836503fffc1674c575b400fc71a8eae886f8f7
Piatan Palar <kpiatan@gmail.com>
2022-07-21 10:43:57 -0300

    new file:   src/lars3.m 	modified:   src/simulate_status.cpp

---
+++ src/lars3.m
@@ -0,0 +1,233 @@
+% ROS Configuration
+
+%rosshutdown;
+%rosinit;
+
+sub(1) = rossubscriber('/status_1');
+sub(2) = rossubscriber('/status_2');
+sub(3) = rossubscriber('/status_3');
+sub(4) = rossubscriber('/status_4');
+sub(5) = rossubscriber('/status_5');
+sub(6) = rossubscriber('/status_12');
+sub(7) = rossubscriber('/status_22');
+sub(8) = rossubscriber('/status_32');
+sub(9) = rossubscriber('/status_42');
+sub(10) = rossubscriber('/status_52');
+sub(11) = rossubscriber('/status_13');
+sub(12) = rossubscriber('/status_23');
+sub(13) = rossubscriber('/status_33');
+sub(14) = rossubscriber('/status_43');
+sub(15) = rossubscriber('/status_53');
+sub_joints = rossubscriber('/joint_states');
+    
+[pub,msg] = rospublisher('/setpoints','movemaster_msg/setpoint');
+[pub2,msg2] = rospublisher('/setpoints2','movemaster_msg/setpoint');
+[pub3,msg3] = rospublisher('/setpoints3','movemaster_msg/setpoint');
+[pub_hanoi,msg_hanoi] = rospublisher('/hanoi_status','std_msgs/Int32MultiArray');
+[pub_hanoi2,msg_hanoi2] = rospublisher('/hanoi_status2','std_msgs/Int32MultiArray');
+[pub_hanoi3,msg_hanoi3] = rospublisher('/hanoi_status3','std_msgs/Int32MultiArray');
+
+vetor_desenho = [1 2 3 7 8 9];
+vetor_desenho2 = [1 2 3 7 8 9];
+vetor_desenho3 = [1 2 3 7 8 9];
+msg_hanoi.Data = vetor_desenho;
+msg_hanoi2.Data = vetor_desenho2;
+msg_hanoi3.Data = vetor_desenho3;
+send(pub_hanoi,msg_hanoi);
+send(pub_hanoi2,msg_hanoi2);
+send(pub_hanoi3,msg_hanoi3);
+
+%% pontos
+
+%robo1
+
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(6,9,10,vetor_desenho)];
+vetor_posicoes = vetor_posicoes_novo;
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(3,3,9,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(2,2,4,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(3,9,5,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(5,8,2,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(6,10,3,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(4,7,10,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(6,3,7,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(5,2,11,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(6,7,12,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(1,1,7,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(6,12,1,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(5,11,8,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(6,1,9,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(4,10,1,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(6,9,10,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(5,8,2,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(3,5,3,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(2,4,8,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(3,3,9,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+[vetor_posicoes_novo, vetor_desenho_novo] = [criapontos(6,10,3,vetor_desenho(end,:))];
+vetor_posicoes = [vetor_posicoes;vetor_posicoes_novo];
+vetor_desenho = [vetor_desenho;vetor_desenho_novo];
+
+% robo2
+
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(3,3,10,vetor_desenho2)];
+vetor_posicoes2 = vetor_posicoes_novo2;
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(2,2,4,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(3,10,5,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(1,1,10,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(3,5,1,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(2,4,11,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(3,1,12,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(6,9,1,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(5,8,4,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(6,1,5,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(4,7,1,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(6,5,7,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(5,4,2,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(6,7,3,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(3,12,7,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(2,11,4,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(3,7,5,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(1,10,7,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(3,5,10,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(2,4,8,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+[vetor_posicoes_novo2, vetor_desenho_novo2] = [criapontos(3,10,9,vetor_desenho2(end,:))];
+vetor_posicoes2 = [vetor_posicoes2;vetor_posicoes_novo2];
+vetor_desenho2 = [vetor_desenho2;vetor_desenho_novo2];
+
+%% IK
+
+for i = 1:length(vetor_posicoes)
+
+    % Home configuration.
+    home = [deg2rad(   0);
+            deg2rad( 106);
+            deg2rad(-123);
+            deg2rad(   0);
+            deg2rad(   0)];
+    
+    % Defines the destination pose.
+    goal_pos = [465;vetor_posicoes(i,1);vetor_posicoes(i,2)];
+    goal_pos2 = [465;vetor_posicoes2(i,1);vetor_posicoes2(i,2)];
+    ori_angle = deg2rad(90); % Calculations in radians, to the Movemaster in degrees.
+    
+    % Computes the inverse kinematics.
+    ori_axis = [0;1;0];
+    goal_ori = [cos(ori_angle/2); sin(ori_angle/2)*ori_axis];
+    [theta, ~, valid] = MovemasterIK_PosAndOri(home, goal_pos, ori_angle);
+    [theta2, ~, valid] = MovemasterIK_PosAndOri(home, goal_pos2, ori_angle);
+    theta_deg = wrapTo180(rad2deg(theta));
+    theta_deg2 = wrapTo180(rad2deg(theta2));
+    
+    if goal_pos(1) < 10
+        valid= false;
+    end
+    
+    if goal_pos(3) < 10
+        valid= false;
+    end
+    valid;
+    
+    % Movemaster
+    
+    if valid
+        msg.GoHome = 0;
+        msg.Set1 = theta_deg(1);
+        msg.Set2 = theta_deg(2);
+        msg.Set3 = theta_deg(3);
+        msg.Set4 = theta_deg(4);
+        msg.Set5 = theta_deg(5) - theta_deg(4);
+
+        msg2.GoHome = 0;
+        msg2.Set1 = theta_deg2(1);
+        msg2.Set2 = theta_deg2(2);
+        msg2.Set3 = theta_deg2(3);
+        msg2.Set4 = theta_deg2(4);
+        msg2.Set5 = theta_deg2(5) - theta_deg2(4);
+    
+        send(pub,msg);
+        send(pub2,msg2);
+
+        msg_hanoi.Data = vetor_desenho(ceil((i+2)/6),:);
+        send(pub_hanoi,msg_hanoi);
+        msg_hanoi2.Data = vetor_desenho2(ceil((i+2)/6),:);
+        send(pub_hanoi2,msg_hanoi2);
+    end
+    pause(0.8);
+end
--- src/simulate_status.cpp
+++ src/simulate_status.cpp
@@ -9,6 +9,8 @@
 #include <cstdlib>
 
 double setpoint_1, setpoint_2, setpoint_3, setpoint_4, setpoint_5;
+double setpoint_12, setpoint_22, setpoint_32, setpoint_42, setpoint_52;
+double setpoint_13, setpoint_23, setpoint_33, setpoint_43, setpoint_53;
 
 void subCallback(const movemaster_msg::setpoint::ConstPtr& rec_msg)
 {
@@ -31,6 +33,26 @@ void subCallback(const movemaster_msg::setpoint::ConstPtr& rec_msg)
     */
 }
 
+void subCallback2(const movemaster_msg::setpoint::ConstPtr& rec_msg2)
+{
+    setpoint_12 = rec_msg2->set_1 * 185;
+    setpoint_22 = rec_msg2->set_2 * 228;
+    setpoint_32 = rec_msg2->set_3 * 186;
+    setpoint_42 = rec_msg2->set_4 * 154.56;
+    setpoint_52 = rec_msg2->set_5 * 116;
+
+}
+
+void subCallback3(const movemaster_msg::setpoint::ConstPtr& rec_msg3)
+{
+    setpoint_13 = rec_msg3->set_1 * 185;
+    setpoint_23 = rec_msg3->set_2 * 228;
+    setpoint_33 = rec_msg3->set_3 * 186;
+    setpoint_43 = rec_msg3->set_4 * 154.56;
+    setpoint_53 = rec_msg3->set_5 * 116;
+
+}
+
 int main(int argc, char** argv) {
     ros::init(argc, argv, "simulate_status");
     ros::NodeHandle n;
@@ -40,7 +62,19 @@ int main(int argc, char** argv) {
     ros::Publisher pub_3 = n.advertise<movemaster_msg::status>("/status_3", 1);
     ros::Publisher pub_4 = n.advertise<movemaster_msg::status>("/status_4", 1);
     ros::Publisher pub_5 = n.advertise<movemaster_msg::status>("/status_5", 1);
+    ros::Publisher pub_12 = n.advertise<movemaster_msg::status>("/status_12", 1);
+    ros::Publisher pub_22 = n.advertise<movemaster_msg::status>("/status_22", 1);
+    ros::Publisher pub_32 = n.advertise<movemaster_msg::status>("/status_32", 1);
+    ros::Publisher pub_42 = n.advertise<movemaster_msg::status>("/status_42", 1);
+    ros::Publisher pub_52 = n.advertise<movemaster_msg::status>("/status_52", 1);
+    ros::Publisher pub_13 = n.advertise<movemaster_msg::status>("/status_13", 1);
+    ros::Publisher pub_23 = n.advertise<movemaster_msg::status>("/status_23", 1);
+    ros::Publisher pub_33 = n.advertise<movemaster_msg::status>("/status_33", 1);
+    ros::Publisher pub_43 = n.advertise<movemaster_msg::status>("/status_43", 1);
+    ros::Publisher pub_53 = n.advertise<movemaster_msg::status>("/status_53", 1);
     ros::Subscriber sub = n.subscribe("/setpoints", 1000, subCallback);
+    ros::Subscriber sub2 = n.subscribe("/setpoints2", 1000, subCallback2);
+    ros::Subscriber sub3 = n.subscribe("/setpoints3", 1000, subCallback3);
     tf::TransformBroadcaster broadcaster;
     ros::Rate loop_rate(30);
 
@@ -49,6 +83,16 @@ int main(int argc, char** argv) {
     movemaster_msg::status pub_msg_3;
     movemaster_msg::status pub_msg_4;
     movemaster_msg::status pub_msg_5;
+    movemaster_msg::status pub_msg_12;
+    movemaster_msg::status pub_msg_22;
+    movemaster_msg::status pub_msg_32;
+    movemaster_msg::status pub_msg_42;
+    movemaster_msg::status pub_msg_52;
+    movemaster_msg::status pub_msg_13;
+    movemaster_msg::status pub_msg_23;
+    movemaster_msg::status pub_msg_33;
+    movemaster_msg::status pub_msg_43;
+    movemaster_msg::status pub_msg_53;
 
     while (ros::ok()) {
 
@@ -77,11 +121,65 @@ int main(int argc, char** argv) {
         pub_msg_5.pulse_count   =   setpoint_5/116;
         pub_msg_5.IsDone = true;
 
+        pub_msg_12.joint         =   "Joint 12";
+        pub_msg_12.setpoint      =   setpoint_12;
+        pub_msg_12.pulse_count   =   setpoint_12/185;
+        pub_msg_12.IsDone = true;
+        pub_msg_22.joint         =   "Joint 22";
+        pub_msg_22.setpoint      =   setpoint_22;
+        pub_msg_22.pulse_count   =   setpoint_22/228;
+        pub_msg_22.IsDone = true;
+        pub_msg_32.joint         =   "Joint 32";
+        pub_msg_32.setpoint      =   setpoint_32;
+        pub_msg_32.pulse_count   =   setpoint_32/186;
+        pub_msg_32.IsDone = true;
+        pub_msg_42.joint         =   "Joint 42";
+        pub_msg_42.setpoint      =   setpoint_42;
+        pub_msg_42.pulse_count   =   setpoint_42/154.56;
+        pub_msg_42.IsDone = true;
+        pub_msg_52.joint         =   "Joint 52";
+        pub_msg_52.setpoint      =   setpoint_52;
+        pub_msg_52.pulse_count   =   setpoint_52/116;
+        pub_msg_52.IsDone = true;
+
+        pub_msg_13.joint         =   "Joint 13";
+        pub_msg_13.setpoint      =   setpoint_13;
+        pub_msg_13.pulse_count   =   setpoint_13/185;
+        pub_msg_13.IsDone = true;
+        pub_msg_23.joint         =   "Joint 23";
+        pub_msg_23.setpoint      =   setpoint_23;
+        pub_msg_23.pulse_count   =   setpoint_23/228;
+        pub_msg_23.IsDone = true;
+        pub_msg_33.joint         =   "Joint 33";
+        pub_msg_33.setpoint      =   setpoint_33;
+        pub_msg_33.pulse_count   =   setpoint_33/186;
+        pub_msg_33.IsDone = true;
+        pub_msg_43.joint         =   "Joint 43";
+        pub_msg_43.setpoint      =   setpoint_43;
+        pub_msg_43.pulse_count   =   setpoint_43/154.56;
+        pub_msg_43.IsDone = true;
+        pub_msg_53.joint         =   "Joint 53";
+        pub_msg_53.setpoint      =   setpoint_53;
+        pub_msg_53.pulse_count   =   setpoint_53/116;
+        pub_msg_53.IsDone = true;
+
         pub_1.publish(pub_msg_1);
         pub_2.publish(pub_msg_2);
         pub_3.publish(pub_msg_3);
         pub_4.publish(pub_msg_4);
         pub_5.publish(pub_msg_5);
+
+        pub_12.publish(pub_msg_12);
+        pub_22.publish(pub_msg_22);
+        pub_32.publish(pub_msg_32);
+        pub_42.publish(pub_msg_42);
+        pub_52.publish(pub_msg_52);
+
+        pub_13.publish(pub_msg_13);
+        pub_23.publish(pub_msg_23);
+        pub_33.publish(pub_msg_33);
+        pub_43.publish(pub_msg_43);
+        pub_53.publish(pub_msg_53);
 
         loop_rate.sleep();
         ros::spinOnce();
