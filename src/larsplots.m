33bf6cf392ffbf500d7b0280493d04bda337caf8
Piatan Palar <kpiatan@gmail.com>
2022-08-01 14:39:02 -0300

    deleted:    launch/display (copy).launch 	new file:   launch/original.launch 	modified:   src/larsplots.m 	renamed:    src/movemaster_urdf (copy).xml -> src/movemaster_urdf1.xml

--- src/larsplots.m
+++ src/larsplots.m
@@ -1,8 +1,12 @@
+load('trajetorias.mat')
+
 sqrt(sumsqr(diff(theta_save(:,1))));
+sum(abs(diff(theta_save(:,1))));
 
 joint = 1;
 
-plot(diff(theta_save(:,joint)))
+plot(diff(theta_save(:,joint)),'b','LineWidth',2)
 hold on
-plot(diff(theta_save2(:,joint)))
-plot(diff(theta_save3(:,joint)))
+plot(diff(theta_save2(:,joint)),'r','LineWidth',2)
+plot(diff(theta_save3(:,joint)),'g','LineWidth',2)
+legend('Algorithm 1','Algorithm 2','Algorithm 3')
