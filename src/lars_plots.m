8246ca22638f455902e6dad78836f0fe9ef62ad0
Piatan Palar <kpiatan@gmail.com>
2022-08-04 10:49:03 -0300

    modified:   src/lars_plots.m

--- src/lars_plots.m
+++ src/lars_plots.m
@@ -1,14 +1,14 @@
 load('trajetorias.mat')
 
-sqrt(sumsqr(diff(theta_save(:,1))));
+%sqrt(sumsqr(diff(theta_save(:,1))));
 sum(abs(diff(theta_save(:,1))));
 
 joint = 1;
 
-plot(abs(diff(theta_save2(:,joint))),'r','LineWidth',2)
+plot(diff(theta_save2(:,joint)),'r','LineWidth',2)
 hold on
-plot(abs(diff(theta_save3(:,joint))),'g','LineWidth',2)
-plot(abs(diff(theta_save4(:,joint))),'y','LineWidth',2)
+plot(diff(theta_save3(:,joint)),'g','LineWidth',2)
+plot(diff(theta_save4(:,joint)),'y','LineWidth',2)
 legend('Algorithm 1','Algorithm 2','Algorithm 3')
 hold off
 
