f9008988031614cc7286ee6f75a8bec09f508e62
Piatan Palar <kpiatan@gmail.com>
2022-08-05 14:12:24 -0300

    modified:   launch/real.launch 	modified:   rviz/display.rviz 	new file:   src/lars_executa_best.m 	modified:   src/lars_executa_trajetorias.m

--- src/lars_executa_best.m
+++ src/lars_executa_best.m
@@ -14,7 +14,7 @@ rosinit;
 
 load('trajetorias.mat')
 
-msg_hanoi.Data = vetor_desenho(1,:);
+msg_hanoi.Data = vetor_desenho3(1,:);
 msg_hanoi2.Data = vetor_desenho2(1,:);
 msg_hanoi3.Data = vetor_desenho3(1,:);
 msg_hanoi4.Data = vetor_desenho4(1,:);
@@ -27,10 +27,7 @@ send(pub_hanoi4,msg_hanoi4);
 
 for i = 1:length(theta_save)
 
-    theta_deg = theta_save(i,:);
-    theta_deg2 = theta_save2(i,:);
-    theta_deg3 = theta_save3(i,:);
-    theta_deg4 = theta_save4(i,:);
+    theta_deg = theta_save3(i,:);
 
     % Movemaster
     
@@ -40,41 +37,11 @@ for i = 1:length(theta_save)
     msg.Set3 = theta_deg(3);
     msg.Set4 = theta_deg(4);
     msg.Set5 = theta_deg(5) - theta_deg(4);
-
-    msg2.GoHome = 0;
-    msg2.Set1 = theta_deg2(1);
-    msg2.Set2 = theta_deg2(2);
-    msg2.Set3 = theta_deg2(3);
-    msg2.Set4 = theta_deg2(4);
-    msg2.Set5 = theta_deg2(5) - theta_deg2(4);
-
-    msg3.GoHome = 0;
-    msg3.Set1 = theta_deg3(1);
-    msg3.Set2 = theta_deg3(2);
-    msg3.Set3 = theta_deg3(3);
-    msg3.Set4 = theta_deg3(4);
-    msg3.Set5 = theta_deg3(5) - theta_deg3(4);
-
-    msg4.GoHome = 0;
-    msg4.Set1 = theta_deg4(1);
-    msg4.Set2 = theta_deg4(2);
-    msg4.Set3 = theta_deg4(3);
-    msg4.Set4 = theta_deg4(4);
-    msg4.Set5 = theta_deg4(5) - theta_deg4(4);
     
     send(pub,msg);
-    send(pub2,msg2);
-    send(pub3,msg3);
-    send(pub4,msg4);
 
-    msg_hanoi.Data = vetor_desenho(ceil((i+2)/6),:);
+    msg_hanoi.Data = vetor_desenho3(ceil((i+2)/6),:);
     send(pub_hanoi,msg_hanoi);
-    msg_hanoi2.Data = vetor_desenho2(ceil((i+2)/6),:);
-    send(pub_hanoi2,msg_hanoi2);
-    msg_hanoi3.Data = vetor_desenho3(ceil((i+2)/6),:);
-    send(pub_hanoi3,msg_hanoi3);
-    msg_hanoi4.Data = vetor_desenho4(ceil((i+2)/6),:);
-    send(pub_hanoi4,msg_hanoi4);
     
     pause(3.5);
     
